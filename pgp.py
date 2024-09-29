57ec94b883c5cd0d7d7edc92a799c46f0ef2dad3
GeonoTRON2000 <geono@thegt.org>
2024-09-29 14:05:28 -0400

    formatting of unencrypted message

--- pgp.py
+++ pgp.py
@@ -7,6 +7,7 @@ from email.mime.application import MIMEApplication
 from copy import deepcopy
 
 keep_headers = []
+strip_headers = ["mime-version", "content-transfer-encoding"]
 
 def encrypt(mime_msg: EmailMessage, recipients: list[str]):
   payload = extract_body(deepcopy(mime_msg))
@@ -21,24 +22,30 @@ def encrypt(mime_msg: EmailMessage, recipients: list[str]):
 
   container = MIMEMultipart(
     "encrypted",
-    protocol="application/pgp-encrypted",
-    _data="This is an OpenPGP/MIME encrypted message (RFC 4880 and 3156)"
+    protocol="application/pgp-encrypted"
   )
+  container.preamble = "This is an OpenPGP/MIME encrypted message (RFC 4880 and 3156)"
+
   part1 = MIMEApplication(
     _data="Version: 1\n",
     _subtype="pgp-encrypted",
     _encoder=email.encoders.encode_7or8bit
   )
   part1["Content-Description"] = "PGP/MIME version identification"
+
   part2 = MIMEApplication(
     _data=str(enc_msg),
-    _subtype="octet-stream; name=encrypted.asc",
+    _subtype="octet-stream; name=\"encrypted.asc\"",
     _encoder=email.encoders.encode_7or8bit
   )
   part2["Content-Description"] = "OpenPGP encrypted message"
   part2["Content-Disposition"] = "inline; filename=\"encrypted.asc\""
+
+  strip_extraneous_headers(part1)
+  strip_extraneous_headers(part2)
   container.attach(part1)
   container.attach(part2)
+  strip_extraneous_headers(container)
   return container, True
 
 def already_encrypted(mime_msg: EmailMessage) -> bool:
@@ -53,6 +60,11 @@ def extract_body(msg: EmailMessage) -> EmailMessage:
   for header in msg.keys():
       del msg[header]
   return msg
+
+def strip_extraneous_headers(msg: EmailMessage) -> None:
+  for header in msg.keys():
+    if header.lower() in strip_headers:
+      del msg[header]
 
 def load_keys(recipients: list[str]) -> list[pgpy.PGPKey]:
   addrs = []
